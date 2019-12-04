49994d1e93db9768b044d4f85664eafad527292c
Maja_Krzyzaniak <maja_krzyzaniak@outlook.com>
2019-12-04 20:18:26 +0100

    Cows_and_bulls_2

--- Section_02/Cows_and_bulls/Cows_and_bulls/Cows_Main.cpp
+++ Section_02/Cows_and_bulls/Cows_and_bulls/Cows_Main.cpp
@@ -1,3 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
 int main() {
+	//introduce the game
+	constexpr int WORLD_LENGHT = 8;
+	cout << "Welcome to bulls and cows" << endl;
+	cout << "Can you guess " << WORLD_LENGHT;
+	cout << " letter I'm thinking of?\n";
+	cout << endl;
+
+	//get a quess from the player
+	cout << "Enter your guess";
+	string Guess = "";
+	cin >> Guess;
+	cout << endl;
+	//repeat the quess back to them
+
+	system("pause");
 	return 0;
 }
