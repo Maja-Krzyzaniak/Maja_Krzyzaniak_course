5f4ce0040ca6e9bb11f0445abb6201a801bde460
Maja_Krzyzaniak <maja_krzyzaniak@outlook.com>
2019-12-13 17:51:08 +0100

    Iterating With For & While Loops / Test 2

--- Section_02/Cows_and_bulls/Cows_and_bulls/Cows_Main.cpp
+++ Section_02/Cows_and_bulls/Cows_and_bulls/Cows_Main.cpp
@@ -1,28 +1,43 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main() {
+
+void PrintIntro();
+string GetguessAndPrintBack();
+
+//the entry point of our application
+int main()
+{
+	PrintIntro();
+	
+	//loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS =5;
+	for (int count = 1; count <=NUMBER_OF_TURNS; count++)
+	{
+		GetguessAndPrintBack();
+		cout << endl;
+	}
+
+	return 0;
+}
+void PrintIntro()
+{
 	//introduce the game
-	constexpr int WORLD_LENGHT = 8;
-	cout << "Welcome to bulls and cows" << endl;
+	constexpr int WORLD_LENGHT = 9;
+	cout << "Welcome to bulls and cows.\n";
 	cout << "Can you guess " << WORLD_LENGHT;
 	cout << " letter I'm thinking of?\n";
 	cout << endl;
-
+	return ;
+}
+string GetguessAndPrintBack()
+{
 	//get a quess from the player
 	cout << "Enter your guess ";
 	string Guess = "";
-	getline (cin,Guess);
-
-	//repeat the quess back to them
-	cout << "Your guess was " << Guess << endl;
-
-	//get a quess from the player
-	cout << "Enter your guess ";
 	getline(cin, Guess);
 
-	//repeat the quess back to them
+	//print the quess back
 	cout << "Your guess was " << Guess << endl;
-	system("pause");
-	return 0;
+	return Guess;
 }
