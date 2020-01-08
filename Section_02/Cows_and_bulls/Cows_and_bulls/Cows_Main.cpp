86bfe77776dffcbd7a25796cf6c34baf8daff34c
Maja_Krzyzaniak <maja_krzyzaniak@outlook.com>
2020-01-08 13:55:38 +0100

    no message

--- Section_02/Cows_and_bulls/Cows_and_bulls/Cows_Main.cpp
+++ Section_02/Cows_and_bulls/Cows_and_bulls/Cows_Main.cpp
@@ -1,43 +1,62 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 void PrintIntro();
-string GetguessAndPrintBack();
+void PlayGame();
+string GetGuess();
+bool AskToPlayAgain();
 
-//the entry point of our application
-int main()
+void PlayGame()
 {
-	PrintIntro();
-	
-	//loop for the number of turns asking for guesses
-	constexpr int NUMBER_OF_TURNS =5;
-	for (int count = 1; count <=NUMBER_OF_TURNS; count++)
-	{
-		GetguessAndPrintBack();
+	// loop for the number of turns asking for guesses	
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
+{
+
+		string Guess = GetGuess();
+		cout << "Your guess was: " << Guess << endl;
 		cout << endl;
-	}
-
-	return 0;// exit the application
 }
-void PrintIntro()
-{
-	//introduce the game
-	constexpr int WORLD_LENGHT = 9;
-	cout << "Welcome to bulls and cows.\n";
-	cout << "Can you guess " << WORLD_LENGHT;
-	cout << " letter I'm thinking of?\n";
-	cout << endl;
-	return ;
+	
 }
-string GetguessAndPrintBack()
+string GetGuess()
 {
 	//get a quess from the player
 	cout << "Enter your guess ";
 	string Guess = "";
 	getline(cin, Guess);
-
-	//print the quess back
-	cout << "Your guess was " << Guess << endl;
 	return Guess;
 }
+
+bool AskToPlayAgain()
+{
+	return false;
+}
+
+void PrintIntro()
+{
+	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << std::endl;
+	std::cout << "          }   {         ___ " << std::endl;
+	std::cout << "          (o o)        (o o) " << std::endl;
+	std::cout << "   /-------\\ /          \\ /-------\\ " << std::endl;
+	std::cout << "  / | BULL |O            O| COW  | \\ " << std::endl;
+	std::cout << " *  |-,--- |              |------|  * " << std::endl;
+	std::cout << "    ^      ^              ^      ^ " << std::endl;
+	std::cout << " Can you guess a letter I'm thinking of?\n";
+	std::cout << std::endl;
+}
+
+//the entry point of our application
+int main()
+{
+	PrintIntro();
+	PlayGame();
+	AskToPlayAgain();
+	system("pause");
+	return 0;// exit the application
+}
+
+	
